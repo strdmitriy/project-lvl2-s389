9eb47f697c6bb93a22c15e105f44a1dfe7e22b37
strungar.dv <dmitrii.strungar@domru.ru>
2019-01-01 23:51:38 +0500

    adding help

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 import program from 'commander';
+import version from '../../package.json';
 
 program
-  .version('0.0.1')
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.3')
+  .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
