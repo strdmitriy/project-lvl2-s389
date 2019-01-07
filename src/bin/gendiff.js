04ca25465b008898958dd0e5add566454107d6c5
strungar.dv <dmitrii.strungar@domru.ru>
2019-01-07 20:48:09 +0500

    fix for eslint

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,5 +7,6 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.3')
   .option('-f, --format [type]', 'Output format')
-  .action((firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig, program.format)))
+  .action((firstConfig, secondConfig) => (
+    console.log(genDiff(firstConfig, secondConfig, program.format))))
   .parse(process.argv);
